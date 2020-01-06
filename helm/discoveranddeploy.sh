8630b571392dd366c2c9e667460d20c9ce2d7499
Karim Vaes <Karim.Vaes@microsoft.com>
2020-01-06 20:16:55 +0000

    helm update

--- helm/discoveranddeploy.sh
+++ helm/discoveranddeploy.sh
@@ -31,7 +31,7 @@ function deployToCluster {
   getClusterKubectl $clustername $clusterrg
   helm init --upgrade --service-account default
   kubectl -n kube-system wait --for=condition=Ready pod -l name=tiller --timeout=300s
-  echo "helm install $deploymentname 'vmchooserregistry/vmchooserbackend' --version $version -n $namespace -f $valuesfile"
+  helm repo update
   helm install 'vmchooserregistry/vmchooserbackend' --name "$deploymentname" --version "$version" -n "$namespace" -f "$valuesfile"
 }
 
