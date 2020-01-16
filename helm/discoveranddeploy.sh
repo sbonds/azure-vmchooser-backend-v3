9ccabebe16f9f0566b7121e1262f90e25554b7c7
Karim Vaes <Karim.Vaes@microsoft.com>
2020-01-16 20:20:01 +0000

    added tm to deploy script

--- helm/discoveranddeploy.sh
+++ helm/discoveranddeploy.sh
@@ -6,6 +6,7 @@ version=$4
 chart=$5
 deploymentname=$6 
 valuesfile=$7
+helmregistry='vmchooserregistry/vmchooserbackend'
 az extension add --name resource-graph
 
 # functions
@@ -31,7 +32,27 @@ function deployToCluster {
   getClusterKubectl $clustername $clusterrg
   lastversion=`helm history "$deploymentname" | tail -n 1 | awk '{ print $1 }'`
   helm status "$deploymentname" | grep -i deployed && action="upgrade" || action="install"
-  helm $action "$deploymentname" 'vmchooserregistry/vmchooserbackend' --version "$version" -n "$namespace" -f "$valuesfile" || helm rollback "$deploymentname" "$lastversion"
+  helm $action "$deploymentname" "$helmregistry" --version "$version" -n "$namespace" -f "$valuesfile" || helm rollback "$deploymentname" "$lastversion"
+  addService2TrafficeManager $clustername
+}
+
+function getTrafficManager {
+  temp=`az graph query -q "Resources | where type =~ \"Microsoft.Network/trafficManagerProfiles\" | where tags[\"Environment\"] =~ \"$environment\" | where tags[\"Workload\"] =~ \"$workload\" | project name" -o yaml | awk '{ print $3 }'`
+  echo $temp
+}
+
+function getTrafficManagerResourcegroup {
+  temp=`az graph query -q "Resources | where type =~ \"Microsoft.Network/trafficManagerProfiles\" | where name =~ \"$1\" | project resourceGroup" -o yaml | awk '{ print $3 }'`
+  echo $temp
+}
+
+function addService2TrafficeManager {
+  pip=`kubectl get services | grep -i "$deploymentname" | awk '{ print $4 }'`
+  clustername=$1
+  tmname=$(getTrafficManager)
+  tmrg=$(getTrafficManagerResourcegroup $tmname)
+  weight=1
+  az network traffic-manager endpoint create --resource-group "$tmrg" --profile-name "$tmname" --type externalEndpoints --name "$clustername" --target "$pip" --weight $weight
 }
 
 # main runtime
