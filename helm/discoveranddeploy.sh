625f374428e4c8d28aa1724188bb65c4c901f0b5
Karim Vaes <Karim.Vaes@microsoft.com>
2020-01-26 21:16:39 +0000

    Filter helm history on deployed status

--- helm/discoveranddeploy.sh
+++ helm/discoveranddeploy.sh
@@ -31,7 +31,7 @@ function deployToCluster {
   clustername=$1
   clusterrg=$(getClusterResourcegroup $clustername)
   getClusterKubectl $clustername $clusterrg
-  lastversion=`helm history "$deploymentname" | tail -n 1 | awk '{ print $1 }'`
+  lastversion=`helm history "$deploymentname" | grep -i 'deployed' | tail -n 1 | awk '{ print $1 }'`
   helm status "$deploymentname" | grep -i deployed && action="upgrade" || action="install"
   helm $action "$deploymentname" "$helmregistry" --version "$version" -n "$namespace" -f "$valuesfile" || helm rollback "$deploymentname" "$lastversion"
   # addService2TrafficeManager $clustername (commented out due to the usage of an ingress controller that will handle this endpoint)
